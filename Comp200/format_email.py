869d99e044a12846d966d83daf6a41a321c72edb
Omar Hussain <omar.hussain@gmail.com>
2019-04-25 00:53:15 -0600

    comp200 TDD: wrote function code, all tests pass

--- Comp200/format_email.py
+++ Comp200/format_email.py
@@ -3,4 +3,4 @@ def email(first, last):
     Receives two string parameters for first & last name,
     Returns an all-lowercase 'first.last@evolveu.ca' email address
     """
-    return ''
+    return f'{first.lower()}.{last.lower()}@evolveu.ca'
