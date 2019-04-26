6123c5903d130250a72828056f00c811bd1ca233
Omar Hussain <omar.hussain@gmail.com>
2019-04-25 18:33:55 -0600

    corrected error in docstring

--- Comp220/read_file.py
+++ Comp220/read_file.py
@@ -1,6 +1,5 @@
 def read_file():
     """
-    Receives one numeric parameter for income,
     Reads file 'syntax.html' from the current directory, and returns a string:
     "Lines: ##, 'else' statements: ##, characters: ##"
     """
