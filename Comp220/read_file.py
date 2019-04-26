2d5907e19dc8320919a9d530b78fc670654967c6
Omar Hussain <omar.hussain@gmail.com>
2019-04-25 18:32:18 -0600

    comp220 file-reading TDD: wrote test and function stub - tests fail as expected

---
+++ Comp220/read_file.py
@@ -0,0 +1,7 @@
+def read_file():
+    """
+    Receives one numeric parameter for income,
+    Reads file 'syntax.html' from the current directory, and returns a string:
+    "Lines: ##, 'else' statements: ##, characters: ##"
+    """
+    return ''
---
+++ Comp220/test_read_file.py
@@ -0,0 +1,4 @@
+from read_file import read_file
+ 
+def test_read_file():
+    assert read_file() == 'Lines: 214, \'else \' statements: 2, characters: 9177'
