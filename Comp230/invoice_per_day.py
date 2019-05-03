aec29eca0b09e29f7f229208a987e3b6cdfbe6b7
Omar Hussain <omar.hussain@gmail.com>
2019-05-03 06:49:09 -0600

    removed formatting from spreadsheet, .gitignore'd LibreOffice lock files, added comments to code

--- Comp230/invoice_per_day.py
+++ Comp230/invoice_per_day.py
@@ -5,59 +5,30 @@ wk = openpyxl.load_workbook('sales_data.xlsx')
 inv_wksheet= wk['Invoices']
 line_items_wksheet= wk['Inv Line Items']
 
-print(inv_wksheet.max_row)
 
+# Issue with using max_row as iteration limit:
+#    spreadsheet has *several* empty rows...
 rows = inv_wksheet.max_row
 columns = inv_wksheet.max_column
 
-dates_by_inv={}
 
+# Iterate over rows in worksheet, build dictionary of:
+#   invoice Date with Invoice_ID as key
+dates_by_inv={}
 for i in range(2,47+1):
         dates_by_inv[inv_wksheet.cell(i,2).value] = inv_wksheet.cell(i,3).value
         
 # print(dates_by_inv)
 
+
+# Iterate over rows in worksheet, build dictionary with Invoice_ID as key.
+# Each key has as value a list of dictionaries, with:
+#   Product_ID as key & Units as value
 line_items_by_inv={}
 for i in range(2,66+1):
-        # line_items_by_inv[line_items_wksheet.cell(i,1).value] = line_items_wksheet.cell(i,2).value
         if line_items_wksheet.cell(i,1).value in line_items_by_inv:
                 line_items_by_inv[line_items_wksheet.cell(i,1).value].append({str(line_items_wksheet.cell(i,2).value): line_items_wksheet.cell(i,3).value})
         else:
                 line_items_by_inv[line_items_wksheet.cell(i,1).value] = [{str(line_items_wksheet.cell(i,2).value): line_items_wksheet.cell(i,3).value}]
 
 print(line_items_by_inv)
-
-
-
-
-
-
-
-
-
-
-# #FIND OUT VALUE OF A CEL 
-# #workbook level 
-# wk = openpyxl.load_workbook('data.xlsx')
-# #inv_wksheeteet level
-# sh = wk['Sheet1']
-# #get value of a cell
-# print(sh['A3'].value)
-# print(sh['B4'].value)
-
-# #another method to fetch value
-# c1 = sh.cell(1,1) #first you create cell value # row 1, column 1
-# print(c1.value)
-
-# # READ ALL THE DATA IN AN EXCEL FILE  
-# #find how many rows there are
-
-# rows = sh.max_row #finds out how many rows there are
-# columns = sh.max_column
-# print(rows)
-# print(columns)
-
-# for i in range(1,rows+1):
-#     for j in range(1, columns+1):
-#         c = sh.cell(i,j)
-#         print(c.value)
