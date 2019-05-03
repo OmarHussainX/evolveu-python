967a0d0da8d0d65e1f4e751ae156d6a2ea4fb9f3
Omar Hussain <omar.hussain@gmail.com>
2019-05-03 16:16:10 -0600

    changing approach, competency requirements changed/clarified, developing function(s) to generate invoice given invoice ID

--- Comp230/invoice_per_day.py
+++ Comp230/invoice_per_day.py
@@ -1,3 +1,32 @@
+# our data model is
+# table customers: $1 - id, $2 - first name, $3 - last name
+# table invoices: $1 - id, $2 - customer, $ - date
+# table products: $1 - id, $2 - name, $3 - price, $4 inventory
+# table line sales: $1 - id, $2 - product, $ - quantity
+
+# reports we need:
+# total amount invoiced on a given date
+def generate_invoice (invoice_id):
+        # determine customer name and invoice date from invoice_id from the invoices table
+        # build a header that shows invoice id, date, and customer
+        # initialize grand total as 0
+        # loop through line item for this invoice id
+        #   determine product name and price from the line item
+        #   calculate line total
+        #   add a line to invoice with product name, price, quantity and subtotal line
+        #   add subtotal to grand total
+        # end the grand total line with the grand total that we have already calculated
+        # return what we have created
+
+
+
+    return 0
+
+
+
+
+
+
 """
 TODO
 
---
+++ Comp230/tests.py
@@ -0,0 +1,5 @@
+# test description
+
+# invoices on April 25 should equal 10
+
+# invoices by Client 
