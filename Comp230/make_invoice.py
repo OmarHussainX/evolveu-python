29519673ef48d60813761bd8503c31d092e67717
Omar Hussain <omar.hussain@gmail.com>
2019-05-07 17:22:04 -0600

    refactored code, improved comments

--- Comp230/make_invoice.py
+++ Comp230/make_invoice.py
@@ -1,70 +1,91 @@
 import openpyxl
 
 
-# table customers: $1 - id, $2 - first name, $3 - last name
-# table invoices: $1 - id, $2 - customer, $ - date
-# table products: $1 - id, $2 - name, $3 - price, $4 inventory
-# table line sales: $1 - id, $2 - product, $ - quantity
-
 def make_invoice(invoice_id):
-        # determine customer name and invoice date from invoice_id from the invoices table
-        # build a header that shows invoice id, date, and customer
-        # initialize grand total as 0
-        # loop through line item for this invoice id
-        #   determine product name and price from the line item
-        #   calculate line total
-        #   add a line to invoice with product name, price, quantity and subtotal line
-        #   add subtotal to grand total
-        # end the grand total line with the grand total that we have already calculated
-        # return what we have created
+    """
+    Receives an integer (invoice id) as parameter, obtains
+    invoice data from a spreadhseet ('sales_data.xlsx') and
+    produces an invoice with the following format:
+
+    Andrew Wang (client ID: 124)
+                                           Invoice #  266       
+                                        Invoice date  2019-04-30
+
+
+      Quantity  Item                 Unit price       Line total
+      --------  ----                 ----------       ----------
+         9      Pen                       70.00       $   630.00
+         2      Stapler                   40.00       $    80.00
+         1      Binder                    10.00       $    10.00
+
+
+                                                      ----------
+                                          TOTAL       $   720.00
+    """
+    # - Open 'sales_data.xlsx', obtain iterators for worksheets of interest
+    # - Use 'invoice_id' to obtain customer id and invoice date
+    # - Use customer id to obtain customer's name
+    #
+    # Create invoice:
+    # - add header showing customer name, id and invoice id, date
+    # - initialise invoice total to 0
+    # - loop through line items for the invoice with id 'invoice_id'
+    #       * determine product id, units sold from the line item
+    #       * use product id to obtain product name, unit price
+    #       * calculate line total (units sold * unit price)
+    #       * add line total to invoice total
+    #       * add a line to invoice showing units sold, product name,
+    #         unit price, and line total
+    # - add footer showing invoice total
 
     wb = openpyxl.load_workbook('sales_data.xlsx')
 
-    cust_wksheet = wb['customers']
-    inv_wksheet = wb['invoices']
-    line_items_wksheet = wb['invoice line items']
-    products_wksheet = wb['products']
+    customers_ws = wb['customers']
+    invoices_ws = wb['invoices']
+    line_items_ws = wb['invoice line items']
+    products_ws = wb['products']
 
-    # Iterate over rows in worksheet, build dictionary of:
-    #   unit Price with Product_ID as key
-    products_by_id = {}
-    for i in range(2, products_wksheet.max_row + 1):
-            if not (products_wksheet.cell(i, 1).value is None):
-                products_by_id[products_wksheet.cell(i, 1).value] = \
-                    {'price': products_wksheet.cell(i, 3).value,
-                     'name': products_wksheet.cell(i, 2).value}
-
-    invoice_txt = ''
-
-    # get customer id, invoice date
+    invoice_txt = ''        # stores text of the entire invoice
+    customer_name = None
     customer_id = None
     invoice_date = None
-    for i in range(2, inv_wksheet.max_row + 1):
-            if not (inv_wksheet.cell(i, 1).value is None):
-                if (invoice_id == inv_wksheet.cell(i, 1).value):
-                    customer_id = inv_wksheet.cell(i, 3).value
-                    invoice_date = inv_wksheet.cell(i, 2).value\
+    invoice_total = 0
+    products = {}           # dictionary of products
+    line_items = {}         # dictionary of invoice line items
+
+    # Obtain customer id and invoice date from 'invoices' worksheet
+    for i in range(2, invoices_ws.max_row + 1):
+            if not (invoices_ws.cell(i, 1).value is None):
+                if (invoice_id == invoices_ws.cell(i, 1).value):
+                    customer_id = invoices_ws.cell(i, 3).value
+                    invoice_date = invoices_ws.cell(i, 2).value\
                         .strftime('%Y-%m-%d')
 
     if customer_id is None:
         return
 
-    # get customer name
-    customer_name = ''
-    for i in range(2, cust_wksheet.max_row + 1):
-            if not (cust_wksheet.cell(i, 1).value is None):
-                if (customer_id == cust_wksheet.cell(i, 1).value):
-                    customer_name = cust_wksheet.cell(i, 2).value + \
-                        ' ' + cust_wksheet.cell(i, 3).value
+    # Obtain customer name from 'customers' worksheet
+    for i in range(2, customers_ws.max_row + 1):
+            if not (customers_ws.cell(i, 1).value is None):
+                if (customer_id == customers_ws.cell(i, 1).value):
+                    customer_name = customers_ws.cell(i, 2).value + \
+                        ' ' + customers_ws.cell(i, 3).value
 
+    # Iterate over rows in 'products' worksheet, build dictionary of
+    # product name and unit price, with product ID as key
+    for i in range(2, products_ws.max_row + 1):
+            if not (products_ws.cell(i, 1).value is None):
+                products[products_ws.cell(i, 1).value] = \
+                    {'name': products_ws.cell(i, 2).value,
+                     'price': products_ws.cell(i, 3).value}
 
-    # get invoice line items
-    line_items = {}
-    for i in range(2, line_items_wksheet.max_row + 1):
-        if not (line_items_wksheet.cell(i, 1).value is None):
-            if (invoice_id == line_items_wksheet.cell(i, 2).value):
-                line_items[line_items_wksheet.cell(i, 3).value] = \
-                    line_items_wksheet.cell(i, 4).value
+    # Iterate over rows in 'invoice line items' worksheet, build dictionary of
+    # units sold, with product ID as key
+    for i in range(2, line_items_ws.max_row + 1):
+        if not (line_items_ws.cell(i, 1).value is None):
+            if (invoice_id == line_items_ws.cell(i, 2).value):
+                line_items[line_items_ws.cell(i, 3).value] = \
+                    line_items_ws.cell(i, 4).value
 
     invoice_txt += f'\
 {customer_name} (client ID: {customer_id})\n\
@@ -90,15 +111,13 @@ def make_invoice(invoice_id):
 {format("----------", ">11")}\
 {format("----------", ">17")}\n'
 
-    grand_total = 0
-    line_total = 0
-    for k, v in line_items.items():
-        line_total = v * products_by_id[k]['price']
-        grand_total += line_total
+    for prod_id, units_sold in line_items.items():
+        line_total = units_sold * products[prod_id]['price']
+        invoice_total += line_total
         invoice_txt += f'\
-{format(v, "^12")}\
-{format(products_by_id[k]["name"], "20")}\
-{format(products_by_id[k]["price"], ">11,.2f")}\
+{format(units_sold, "^12")}\
+{format(products[prod_id]["name"], "20")}\
+{format(products[prod_id]["price"], ">11,.2f")}\
 {format("$", ">8")}\
 {format(line_total, ">9,.2f")}\n'
 
@@ -107,7 +126,7 @@ def make_invoice(invoice_id):
 {format("----------", ">11")}\n\
 {format("TOTAL", ">43")}\
 {format("$", ">8")}\
-{format(grand_total, "9,.2f")}\n'
+{format(invoice_total, "9,.2f")}\n'
 
     print(invoice_txt)
 
