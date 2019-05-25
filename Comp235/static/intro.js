2c32d10a0d21abf6961c8f18e803257bff6bea49
Omar Hussain <omar.hussain@gmail.com>
2019-05-24 18:33:06 -0600

    added async JS function to load HTML data from '/info' route

--- Comp235/static/intro.js
+++ Comp235/static/intro.js
@@ -1,1 +1,25 @@
-console.log(`'Hello world' from Flask server`);
+console.log(`--> 'Hello world' from Flask server`)
+
+
+window.onload = onWindowHasLoaded
+async function onWindowHasLoaded() {
+    console.log(`--> 'intro.html' has finished loading`)
+ 
+    let response
+ 
+    try {
+        response = await fetch('/info')
+
+        if (response.status != 200)
+            throw 'Invalid HTTP Response: ' + response.status
+
+        console.log(`--> Response: ${response}, status: ${response.status}`)
+
+        const data = await response.text()
+        console.log(`--> Data: ${data}`)
+ 
+    } catch (error) {
+        console.error(`--> *** onWindowHasLoaded error: ${error} ***`)
+        console.error(`--> Response: ${response}, status: ${response.status}`)
+    }
+}
