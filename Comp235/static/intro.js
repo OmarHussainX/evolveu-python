7b95a2b5dd767eb85b313f0480fc9db1686c61c0
Omar Hussain <omar.hussain@gmail.com>
2019-05-26 01:49:45 -0600

    expirementing with receiving JSON Object from Flask server, converting it to a String on the HTML+JS front-end, and displaying it in the front-end

--- Comp235/static/intro.js
+++ Comp235/static/intro.js
@@ -1,4 +1,4 @@
-console.log(`--> 'Hello world' from Flask server`)
+console.log(`--> 'Hello world' from Flask server 'front-end'`)
 
 
 window.onload = onWindowHasLoaded
@@ -13,11 +13,20 @@ async function onWindowHasLoaded() {
         if (response.status != 200)
             throw 'Invalid HTTP Response: ' + response.status
 
-        console.log(`--> Response: ${response}, status: ${response.status}`)
+        console.log(`--> Response: ${response}, status: ${response.status}, class of response Object: ${response.constructor.name}`)
 
-        const data = await response.text()
-        console.log(`--> Data:\n[${data}]\ntype: [${typeof data}]`)
+        // const data = await response.text()
+        // console.log(`--> Data rec'd:\n${data}\ndata type: ${typeof data}`)
  
+        const data = await response.json()
+        console.log(`--> data rec'd from response.json():\n${data}\ndata type: ${typeof data}`)
+
+        const dataString = JSON.stringify(data)
+        console.log(`--> data after processing via JSON.stringify():\n[${dataString}]\ndata type: ${typeof dataString}, class: ${dataString.constructor.name}`)
+
+        // display data recieved from server on the front-end
+        document.getElementById('response_data').innerText = dataString
+
     } catch (error) {
         console.error(`--> *** onWindowHasLoaded error: ${error} ***`)
         console.error(`--> Response: ${response}, status: ${response.status}`)
