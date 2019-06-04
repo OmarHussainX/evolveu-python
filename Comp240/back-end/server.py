fe6464709453723a7bcb918013b62cf0bd088d8b
Omar Hussain <omar.hussain@gmail.com>
2019-06-04 16:31:25 -0600

    added basic dump of spreadhseet data to '/datadump' route

--- Comp240/back-end/server.py
+++ Comp240/back-end/server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import os
+import pandas
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 # Configure a secret SECRET_KEY
@@ -9,6 +11,31 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 @app.route('/', methods=['GET'])
 def index():
     return '<h1>Comp240 flask server...</h1>'
+
+
+@app.route('/datadump', methods=['GET'])
+def data_dump():
+    # load 'customers' worksheet into a DataFrame object, and
+    # drop empty rows
+    df = pandas.read_excel('./sales_data.xlsx', sheet_name='customers')
+    df.dropna(inplace=True)
+    print(f'DataFrame (type: {type(df)}) generated from spreadsheet:\n{df}')
+
+    # convert pandas DataFrame to JSON
+    resp_data = df.to_json(orient='records')
+    # resp_data = df.to_json(orient='records', lines=True)
+    print(f'DataFrame data (type: {type(resp_data)}) after conversion to JSON string:\n{resp_data}')
+
+    # create a Flask Response object
+    resp = jsonify(resp_data)
+
+    print(f'--> \'200 OK\' Response object (containing JSON string), sent from\
+\'/datadump()\' route:\n\
+resp:\n{resp}\n\
+type of resp: {type(resp)}\n\
+resp.response:\n{resp.response}\n\
+type of resp.response : {type(resp.response )}\n')
+    return resp, 200
 
 
 if __name__ == '__main__':
