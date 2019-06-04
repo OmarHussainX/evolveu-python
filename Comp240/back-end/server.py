7c0d50584a504fbe3cc271f0bab81613ab27da74
Omar Hussain <omar.hussain@gmail.com>
2019-06-04 17:46:42 -0600

    succeeded in returning properly formatted JSON string in response to GET request on 'datadump'...

--- Comp240/back-end/server.py
+++ Comp240/back-end/server.py
@@ -1,5 +1,4 @@
 import os
-import json
 import pandas
 from flask import Flask, jsonify
 
@@ -16,38 +15,41 @@ def index():
 
 @app.route('/datadump', methods=['GET'])
 def data_dump():
-    # load 'customers' worksheet into a DataFrame object, and
+    # load 'customers' worksheet into a DataFrame object,
+    # preserving data as stored in Excel - i.e. no interpretation of the types
+    # of data in the columns
+    df = pandas.read_excel('./sales_data.xlsx',
+                           sheet_name='customers',
+                           dtype=object)
+    
     # drop empty rows
-    df = pandas.read_excel('./sales_data.xlsx', sheet_name='customers')
     df.dropna(inplace=True)
-    print(f'DataFrame (type: {type(df)}) generated from spreadsheet:\n{df}')
+    
+    print(f'\nDataFrame (type: {type(df)}) generated from spreadsheet:\n{df}')
 
-    # convert pandas DataFrame to JSON
+    # convert pandas DataFrame to JSON string
     resp_data = df.to_json(orient='records')
 
-    print(f'DataFrame data (type: {type(resp_data)}) after conversion to JSON string:\n{resp_data}')
-    
-    # this returns a JSON string, but Content-Type in response headers
-    # is text/html instead of application/json
-    return resp_data
+    print(f'\nDataFrame data (type: {type(resp_data)}) after conversion to JSON \
+string:\n{resp_data}')
 
-    # trying to formaat JSON properly...
-    # this didn't work
-    # alt_resp_data = json.dumps(resp_data)
-    # # resp_data = df.to_json(orient='records', lines=True)
-    # print(f'DataFrame data (type: {type(alt_resp_data)}) after json.dumps():\n{alt_resp_data}')
+    # NOTE: no need to create a Flask Response object via jsonify():
+    #   resp = jsonify(resp_data)
+    #   return resp, 200
+    # jsonify() serializes data passed to it, and the response data has already
+    # been serialized (converted) into a JSON string in this case
 
-    # create a Flask Response object
-    resp = jsonify(resp_data)
-
-    print(f'--> \'200 OK\' Response object (containing JSON string), sent from\
+    # create Flask Response object
+    resp = app.response_class(response=resp_data,
+                              status=200,
+                              mimetype='application/json')
+    print(f'\n--> \'200 OK\' Response object (containing JSON string data), sent from\
 \'/datadump()\' route:\n\
 resp:\n{resp}\n\
 type of resp: {type(resp)}\n\
 resp.response:\n{resp.response}\n\
 type of resp.response : {type(resp.response )}\n')
-    return resp, 200
-
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
