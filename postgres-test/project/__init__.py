414b3a8860600e3741fe7e8a685ab051fdcc53d3
Omar Hussain <omar.hussain@gmail.com>
2019-06-10 20:55:21 -0600

    using pandas to load data from spreadsheet -> DataFrame -> postgres table

--- postgres-test/project/__init__.py
+++ postgres-test/project/__init__.py
@@ -3,6 +3,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import pandas as pd
+# ---------------------------
+# https://stackoverflow.com/a/55495065/11245656
+import csv
+from io import StringIO
+from sqlalchemy import create_engine, MetaData
+
 
 app = Flask(__name__)
 
@@ -22,14 +28,52 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app, db)
 
-
 # load spreadsheet data into a dictionary of DataFrames
+# zeroth column has 'id' for postgres DB
 db_data = pd.read_excel('db_data.xls',
                         sheet_name=['puppies', 'owners'],
-                        # index_col=0,
+                        index_col=0,
                         dtype=object)
 
-print(f'\ndb_data:\n{db_data}')
 
 print(f'\ndb_data["puppies"]:\n{db_data["puppies"]}')
 print(f'\ndb_data["owners"]:\n{db_data["owners"]}')
+
+
+# https://stackoverflow.com/a/55495065/11245656
+def psql_insert_copy(table, conn, keys, data_iter):
+    # gets a DBAPI connection that can provide a cursor
+    dbapi_conn = conn.connection
+    with dbapi_conn.cursor() as cur:
+        s_buf = StringIO()
+        writer = csv.writer(s_buf)
+        writer.writerows(data_iter)
+        s_buf.seek(0)
+
+        columns = ', '.join('"{}"'.format(k) for k in keys)
+        if table.schema:
+            table_name = '{}.{}'.format(table.schema, table.name)
+        else:
+            table_name = table.name
+
+        sql = 'COPY {} ({}) FROM STDIN WITH CSV'.format(
+            table_name, columns)
+        cur.copy_expert(sql=sql, file=s_buf)
+
+engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost/test')
+# metadata = MetaData(engine)
+# metadata.drop_all()
+
+# db_data['puppies'].to_sql('puppies', engine, method=psql_insert_copy)
+
+db_data['puppies'].to_sql('puppies',
+                          engine,
+                          if_exists='replace',
+                          )
+
+db_data['owners'].to_sql('owners',
+                         engine,
+                         if_exists='replace',
+                         )
+
+# db_data['owners'].to_sql('owners', engine, method=psql_insert_copy)
