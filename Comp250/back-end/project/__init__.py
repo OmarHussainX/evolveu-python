4019da1c63bdf5a12b5eb27996ab752993d75eba
Omar Hussain <omar.hussain@gmail.com>
2019-06-16 00:41:46 -0600

    successfully loading dataframes into DB tables, with desired primary keys - need to figure out how to specify foreign keys..

--- Comp250/back-end/project/__init__.py
+++ Comp250/back-end/project/__init__.py
@@ -19,42 +19,80 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+############################################
+#        LOAD RECORDS INTO DATABASE
+############################################
+
 # Load spreadsheet data into a dictionary of DataFrames
-# zeroth column has 'id' for postgres DB
 sales_data = pd.read_excel('sales_data.xlsx',
                            sheet_name=['customers',
                                        'invoices',
                                        'invoice line items',
                                        'products'],
-                           skip_blank_lines=True,
-                           index_col=0)
+                           dtype={
+                               'Customer': db.Integer,
+                               'Invoice': db.Integer,
+                               'Line': db.Integer,
+                               'Product': db.Integer,
+                               'Units': db.Integer,
+                           })
 
 for key in sales_data:
     sales_data[key].dropna(inplace=True)
     print(f'\nsales_data["{key}"]:\n{sales_data[key]}')
+    print(f'\ndata types:\n{sales_data[key].dtypes}')
 
 engine = create_engine(
     'postgresql+psycopg2://postgres:postgres@localhost/sales')
 
-# load DataFrame data into a postgres table
+# Load DataFrame data into a postgres table
+# - do not add the DatFrame index/row numbers as a column
+# - set primary key 'id' as Integer to avoid default 'bigint'
 sales_data['customers'].to_sql('customers',
                                engine,
-                               if_exists='replace')
+                               if_exists='replace',
+                               index=False,
+                               dtype={'Customer': db.Integer})
 
 sales_data['invoices'].to_sql('invoices',
                               engine,
-                              if_exists='replace')
+                              if_exists='replace',
+                              index=False,
+                              dtype={
+                                  'Invoice': db.Integer,
+                                  'Customer': db.Integer})
 
-"""
 sales_data['invoice line items'].to_sql('invoice line items',
-                         engine,
-                         if_exists='replace',
-                         )
+                                        engine,
+                                        if_exists='replace',
+                                        index=False,
+                                        dtype={
+                                            'Line': db.Integer,
+                                            'Invoice': db.Integer,
+                                            'Product': db.Integer,
+                                            'Units': db.Integer})
 
 sales_data['products'].to_sql('products',
-                         engine,
-                         if_exists='replace',
-                         )
- """
+                              engine,
+                              if_exists='replace',
+                              index=False,
+                              dtype={
+                                  'Product': db.Integer,
+                                  'Price': db.Integer})
 
-print(f'tables in postgres DB: {engine.table_names()}')
+# Set specified columns of DataFrame as primary keys in tables
+with engine.connect() as con:
+    # https://gist.github.com/scaryguy/6269293
+    # con.execute('ALTER TABLE "customers" DROP CONSTRAINT "customers_pkey";')
+    con.execute('ALTER TABLE "customers" RENAME COLUMN "Customer" TO "id";')
+    con.execute('ALTER TABLE "customers" ADD PRIMARY KEY (id);')
+    con.execute('ALTER TABLE "invoices" RENAME COLUMN "Invoice" TO "id";')
+    con.execute('ALTER TABLE "invoices" ADD PRIMARY KEY (id);')
+    # con.execute('ALTER TABLE "invoices" ADD CONSTRAINT "invoices_fkey" FOREIGN KEY Customer REFERENCES Customers(id);')
+    con.execute('ALTER TABLE "invoice line items" RENAME COLUMN "Line" TO "id";')
+    con.execute('ALTER TABLE "invoice line items" ADD PRIMARY KEY (id);')
+    con.execute('ALTER TABLE "products" RENAME COLUMN "Product" TO "id";')
+    con.execute('ALTER TABLE "products" ADD PRIMARY KEY (id);')
+
+
+print(f'\ntables in postgres DB: {engine.table_names()}')
