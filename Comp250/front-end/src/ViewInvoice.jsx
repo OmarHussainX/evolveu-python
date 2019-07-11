10ce3977bf3a5de714a979bf594caa896cb5a9ea
Omar Hussain <omar.hussain@gmail.com>
2019-07-11 01:13:43 -0600

    improved appearance when no data has been loaded

--- Comp250/front-end/src/ViewInvoice.jsx
+++ Comp250/front-end/src/ViewInvoice.jsx
@@ -34,50 +34,51 @@ class ViewInvoice extends Component {
 
 
   render() {
-    const { inv_data } = this.state
-    if (inv_data === null) {
-      return (
-        <div>No data...</div>
-      )
-    } else return (
+    const {inv_data} = this.state
+    return (
       <div className='flex-container-col'>
         <div className='panel'>
-          <h1>
-            Invoice #{inv_data.id}
-            <span>(Date: {inv_data.date})</span></h1>
-          <p className='invoice-customer'>
-            {inv_data.customer.first_name} {inv_data.customer.last_name}
-            &nbsp;<span>(Customer id: {inv_data.customer.id})</span>
-          </p>
-          <table className='invoice-details'>
-            <thead>
-              <tr>
-                <th className='units'>Quantity</th>
-                <th className='item'>Item</th>
-                <th>Unit price</th>
-                <th></th>
-                <th>Line total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {inv_data.line_items.map(item => (
-                <tr key={item.id}>
-                  <td className='units'>{item.units}</td>
-                  <td className='item'>{item.product}</td>
-                  <td className='price'>{item.price}</td>
-                  <td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                  <td className='price'>{item.line_total}</td>
+          {inv_data === null ? (
+            <h1>No data...</h1>
+          ) : (<React.Fragment>
+            <h1>
+              Invoice #{inv_data.id}
+              <span>(Date: {inv_data.date})</span>
+            </h1>
+            <p className='invoice-customer'>
+              {inv_data.customer.first_name} {inv_data.customer.last_name}
+              &nbsp;<span>(Customer id: {inv_data.customer.id})</span>
+            </p>
+            <table className='invoice-details'>
+              <thead>
+                <tr>
+                  <th className='units'>Quantity</th>
+                  <th className='item'>Item</th>
+                  <th>Unit price</th>
+                  <th></th>
+                  <th>Line total</th>
                 </tr>
-              ))}
-            </tbody>
-            <tfoot>
-              <tr>
-                <td colSpan={4}>Total</td>
-                <td className='price'>$ {inv_data.total}</td>
-              </tr>
-            </tfoot>
-          </table>
-          <button onClick={this.goBack}>Back</button>
+              </thead>
+              <tbody>
+                {inv_data.line_items.map(item => (
+                  <tr key={item.id}>
+                    <td className='units'>{item.units}</td>
+                    <td className='item'>{item.product}</td>
+                    <td className='price'>{item.price}</td>
+                    <td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                    <td className='price'>{item.line_total}</td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan={4}>Total</td>
+                  <td className='price'>$ {inv_data.total}</td>
+                </tr>
+              </tfoot>
+            </table>
+            <button onClick={this.goBack}>Back</button>
+          </React.Fragment>)}
         </div>
       </div>
     )
